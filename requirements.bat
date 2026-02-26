1d52d56c63d86762f6225fdd83cc0d95e526579a
Michi The Cat <mrzombsoriginal@gmail.com>
2026-02-26 17:33:49 +0300

    Первый старт
    Внятное оформление документов и возможность инициализации сайта

--- requirements.bat
+++ requirements.bat
@@ -2,7 +2,7 @@
 color 0a
 title Course Project - python requirements
 chcp 65001 > nul
-pip install -
+pip install streamlit
 echo --------------------------------------------
 echo  Всё установлено! Если не вылезла ошибка...
 echo --------------------------------------------
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+title Course Project - Site Hosting ;)
+chcp 65001 > nul
+streamlit run app.py
